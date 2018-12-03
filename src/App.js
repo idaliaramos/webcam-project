5f58c51f455992ff4bbe211a7d29c6b3c8bc07c8
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2018-12-02 18:02:15 -0800

    fetched cities from api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import webcam from './webcam.js'
 
 class App extends Component {
+  state={
+  webcam: webcam()
+}
+componentDidMount(){
+  return fetch('http://runningios.com/screamingbox/cameras.json')
+  .then(response=> response.json()).then(data=> this.setState({cities:data})
+)
+}
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
       </div>
     );
   }
