60e57a4bad038bea8373dc99ad42ab601a99c6dc
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2018-12-02 21:28:18 -0800

    made circle have center coordinates of 0,0, and getting the distance moved

--- src/App.js
+++ src/App.js
@@ -16,6 +16,21 @@ componentDidMount(){
 changeCity =(event)=> {
   this.state.webcam.setSource(event.target.id)
 }
+onhover =(e) => {
+  //on hover will move the coordinates
+    let mouseX = e.clientX;
+    let mouseY = e.clientY;
+    //using get bounding client to avoid with issues with nesting
+    let rect = e.target.getBoundingClientRect()
+    let newOriginX= rect.x + rect.width/2
+    let newOriginY= rect.y + rect.height/2
+    let xdistance = mouseX - newOriginX
+    let ydistance = (mouseY - newOriginY )
+    console.log(xdistance, ydistance, 'distance')
+    //call webcam move with coordinates
+    this.state.webcam.move(xdistance, ydistance)
+
+}
 
   render() {
     let image = this.state.webcam.getCameraNode()
@@ -25,7 +40,7 @@ changeCity =(event)=> {
   {/* create a button for each of the camera locations */}
   {this.state.cities.map(city => <button className='btn' id={city.source} onClick={this.changeCity} >{city.name} </button>)}
 <div className='circle-wrapper'>
-  <div  id="circle" onMouseMove={this.onhover} onMouseOut={this.clearCoor}/>
+  <div  id="circle" onMouseMove={this.onhover}/>
 </div>
       </div>
     );
