3faae9c0431b609963cf088a2032a81b424751e1
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2018-12-02 20:05:51 -0800

    rendering image from js file

--- src/App.js
+++ src/App.js
@@ -15,8 +15,10 @@ componentDidMount(){
 
 
   render() {
+    let image = this.state.webcam.getCameraNode()
     return (
       <div className="App">
+        {image}
   {this.state.cities.map(city => <button id={city.source} onClick={this.changeCity} >{city.name} </button>)}
 <div className='circle-wrapper'>
   <div id="circle" onMouseMove={this.onhover} onMouseOut={this.clearCoor}/>
--- src/webcam.js
+++ src/webcam.js
@@ -9,15 +9,19 @@
      * WebCam
      * @constructor
      */
+import React from 'react'
 function webcam() {
-        var webCamContainer = document.createElement("div");
-        webCamContainer.style = "width: 600px; height: 400px; position: relative; overflow: hidden;";
-        var cameraView = document.createElement("img");
-        cameraView.style = "position: absolute; left: 0px; top: 0px; width: auto;";
-        cameraView.src = "http://runningios.com/screamingbox/new-york.jpg";
-        cameraView.id = "cameraView";
-        webCamContainer.appendChild(cameraView);
-
+        // var webCamContainer = document.createElement("div");
+        // webCamContainer.style = "width: 600px; height: 400px; position: relative; overflow: hidden;";
+        // var cameraView = document.createElement("img");
+        // cameraView.style = "position: absolute; left: 0px; top: 0px; width: auto;";
+        // cameraView.src = "http://runningios.com/screamingbox/new-york.jpg";
+        // cameraView.id = "cameraView";
+        // webCamContainer.appendChild(cameraView);
+      let webCamContainer = React.createElement("div",null,
+  React.createElement("img", { id: "cameraView",  src: "http://runningios.com/screamingbox/new-york.jpg" })
+);
+var cameraView = document.getElementById("cameraView");
 
         /**
          *
