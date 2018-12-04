ca223d0aa17d8719a8186f0d0ac1274858969d99
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2018-12-03 21:25:12 -0800

    moving onclick mouse down

--- src/App.js
+++ src/App.js
@@ -16,22 +16,25 @@ componentDidMount(){
 changeCity =(event)=> {
   this.state.webcam.setSource(event.target.id)
 }
-onhover =(e) => {
-  //on hover will move the coordinates
-    let mouseX = e.clientX;
-    let mouseY = e.clientY;
-    //using get bounding client to avoid with issues with nesting
-    let rect = e.target.getBoundingClientRect()
-    let newOriginX= rect.x + rect.width/2
-    let newOriginY= rect.y + rect.height/2
-    let xdistance = mouseX - newOriginX
-    let ydistance = (mouseY - newOriginY )
-    console.log(xdistance, ydistance, 'distance')
-    //call webcam move with coordinates
-    this.state.webcam.move(xdistance, ydistance)
-
+handleMove=(e)=>{
+  e.target.addEventListener('mousemove', this.handleMoving);
 }
-
+handleMoving=(e)=>{
+  let mouseX = e.clientX;
+  let mouseY = e.clientY;
+  //using get bounding client to avoid with issues with nesting
+  let rect = e.target.getBoundingClientRect()
+  let newOriginX= rect.x + rect.width/2
+  let newOriginY= rect.y + rect.height/2
+  let xdistance = mouseX - newOriginX
+  let ydistance = (mouseY - newOriginY )
+  //call webcam move with coordinates
+  this.state.webcam.move(xdistance, ydistance)
+    e.target.addEventListener('mouseout', this.mouseOut);
+}
+mouseOut=(e)=>{
+  console.log('hello')
+}
   render() {
     let image = this.state.webcam.getCameraNode()
     return (
@@ -40,7 +43,7 @@ onhover =(e) => {
   {/* create a button for each of the camera locations */}
   {this.state.cities.map(city => <button className='btn' id={city.source} onClick={this.changeCity} >{city.name} </button>)}
 <div className='circle-wrapper'>
-  <div id="circle" onMouseMove={this.onhover}/>
+  <div id="circle" onMouseDown={this.handleMove}/>
 </div>
       </div>
     );
